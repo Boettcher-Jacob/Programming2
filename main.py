ee4f061410d8f92680ed941ca4bb4e7fd0b7c74c
Boettcher-Jacob <112713125+Boettcher-Jacob@users.noreply.github.com>
2023-01-23 14:37:35 +0000

    Update

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
 print ("Hello, World")
+
+""" Arithmetic Operators
++ Add
+- Subtract
+* Multiply
+/ Divide
+** Pow
+// Floor Divide
+
+"""
+
+"""
+Assignment Operators
+= Value assignment
++= Add to (x += y)
+"""
+"""
+int - Interger
+float - Floating-point number (number w/ decimal)
+str - String (text in quotes)
+bool - Boolean (either true or false)
+chr - Character (single keyboard character, in single quotes)
+
+"""
---
+++ prog52a.py
@@ -0,0 +1,9 @@
+# Lang52a - Area & Perimeter of Rectangle
+length = int(input("Enter length: "))
+width = int(input("Ender width:"))
+
+area = length * width
+perimeter = (length * 2) + (width * 2)
+
+print("Area =", area)
+print("Perimeter =", perimeter)
