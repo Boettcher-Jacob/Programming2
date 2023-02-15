614f362563ebea57d787f046f0b4bed981bea065
Boettcher-Jacob <112713125+Boettcher-Jacob@users.noreply.github.com>
2023-02-15 14:28:37 +0000

    54 b and 54 c

---
+++ prog54c.py
@@ -0,0 +1,9 @@
+Radius = float(input("Input Radius: "))
+
+Pi=3.14159
+Diam = float(Radius) * 2
+Circum = 2 * float(Pi) * float(Radius)
+
+print("Radius is: ", str(Radius))
+print("Diameter is: ", str(Diam))
+print("Circumference is: ", str(Circum))
