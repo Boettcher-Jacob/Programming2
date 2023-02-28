9e3f0fda55b736ad7d6a7b0514903ec93d5dc0d4
Boettcher-Jacob <112713125+Boettcher-Jacob@users.noreply.github.com>
2023-02-28 14:05:21 +0000

    LP4-3 fixed

--- lp4-3.py
+++ lp4-3.py
@@ -1,15 +1,20 @@
-eggs = int(input("Enter number of eggs to be printed: "))
+copies = int(input("Enter the number of copies: "))
 
 price = 0.0
 if copies > 0 and copies <= 99:
   price = 0.30
+  print(price*copies)
 elif copies > 99 and copies <= 499:
   price = 0.28
+  print(price*copies)
 elif copies > 499 and copies <= 749:
   price = 0.27
+  print(price*copies)
 elif copies > 749 and copies <= 1000:
   price = 0.26
-if copies >= 1000:
+  print(price*copies)
+elif copies >= 1000:
   price = 0.25
+  print(price*copies)
 else:
   print("The Number entered is invalid")
