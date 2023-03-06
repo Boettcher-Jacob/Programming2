a1b8349cddec96c83a6b8e73141694403b13a45f
Boettcher-Jacob <112713125+Boettcher-Jacob@users.noreply.github.com>
2023-03-06 14:43:44 +0000

    Commit

--- fileio.py
+++ fileio.py
@@ -1,2 +1,11 @@
 lines = ["Hello, ", "World", "!"]
 
+with open("data/myfile.txt", 'w') as f:
+  # Write data to a file
+  f.write("Hi \n")
+  f.writelines(lines)
+  # or, for line in lines: file.write(line)
+
+with open("data/myfile.txt", 'r') as f:
+  #reading from a file
+  print(f.read())
